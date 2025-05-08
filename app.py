5977bffe9707a6e41e119e1ba7a254574d99a9ae
DevEngTheCloud <96835451+DevEngTheCloud@users.noreply.github.com>
2025-05-08 15:41:43 +0100

    Update app.py
    update changes

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return 'Hello Group, this is workshop 2! changes made!!'
+   return 'Hello Group 1, this is workshop 2! New changes!!'
